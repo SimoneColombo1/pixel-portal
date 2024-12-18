a36823cc68e18ec29ccf7a4c69687abe0a3e8f71
SimoneColombo1 <simcolombo2@gmail.com>
2024-12-18 12:22:56 +0100

    creata la struttura del jumbo

--- app/components/HomePage/Jumbo.js
+++ app/components/HomePage/Jumbo.js
@@ -8,6 +8,12 @@ export default function Jumbo() {
         <div className="image"></div>
         <div className="title">
           <h1>NEWS</h1>
+          <span>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas,
+            nobis ut omnis similique incidunt perspiciatis nulla earum porro
+            magnam totam modi itaque delectus odio accusantium natus voluptate
+            libero, asperiores facilis!
+          </span>
         </div>
       </div>
     </section>
