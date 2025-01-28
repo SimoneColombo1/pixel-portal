fb62c14bc403627c3c0a44c4ea89663d63cc36f1
SimoneColombo1 <simcolombo2@gmail.com>
2025-01-28 11:21:51 +0100

    Creata la struttura di popular slider

--- app/components/HomePage/PopularSlider.js
+++ app/components/HomePage/PopularSlider.js
@@ -18,5 +18,40 @@ export default function PopularSlider() {
     direction: "left",
     rtl: true,
   };
-  return <section className="popular-slider"></section>;
+  return (
+    <section className="popular-slider">
+      <h3>Popular Slider</h3>
+      <div className="slider-container">
+        <Slider {...settings} className="slider">
+          <div className="element">
+            <h3>1</h3>
+          </div>
+          <div className="element">
+            <h3>2</h3>
+          </div>
+          <div className="element">
+            <h3>3</h3>
+          </div>
+          <div className="element">
+            <h3>4</h3>
+          </div>
+          <div className="element">
+            <h3>5</h3>
+          </div>
+          <div className="element">
+            <h3>6</h3>
+          </div>
+          <div className="element">
+            <h3>6</h3>
+          </div>
+          <div className="element">
+            <h3>6</h3>
+          </div>
+          <div className="element">
+            <h3>6</h3>
+          </div>
+        </Slider>
+      </div>
+    </section>
+  );
 }
