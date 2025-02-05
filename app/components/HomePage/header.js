84b036c5a6804eac038f359ab45f59203bedc4cf
SimoneColombo1 <simcolombo2@gmail.com>
2025-02-05 13:27:12 +0100

    Aggiunti i loghi all header e il font per i link

--- app/components/HomePage/header.js
+++ app/components/HomePage/header.js
@@ -5,9 +5,23 @@ export default function Header() {
   return (
     <header>
       <div className="header-container">
-        <div className="logo">LOGO</div>
-        <div className="extended-logo">PiXel Portal</div>
-        <div className="links">
+        <div className="logo">
+          <Image
+            src="/images/Home/small-logo.png"
+            alt="small logo"
+            width={40}
+            height={40}
+          />
+        </div>
+        <div className="extended-logo">
+          <Image
+            src="/images/Home/extended-logo.png"
+            alt="extended logo"
+            width={100}
+            height={40}
+          />
+        </div>
+        <div className="links ">
           <a href="">Home</a>
           <a href="">Search</a>
           <a href="">News</a>
