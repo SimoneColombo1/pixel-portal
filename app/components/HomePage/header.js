cb19ccfbb51b24fdcefded5256ce76517b8d53d2
SimoneColombo1 <simcolombo2@gmail.com>
2025-02-06 11:53:11 +0100

    reso responsive l'header

--- app/components/HomePage/header.js
+++ app/components/HomePage/header.js
@@ -1,9 +1,15 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
-import Style from "../../style/header.scss";
+import { Menu, X } from "lucide-react";
+import "../../style/header.scss";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <header>
+    <header className="header">
       <div className="header-container">
         <div className="logo">
           <Image
@@ -13,7 +19,8 @@ export default function Header() {
             height={40}
           />
         </div>
-        <div className="extended-logo">
+
+        <div className="extended-logo ">
           <Image
             src="/images/Home/extended-logo.png"
             alt="extended logo"
@@ -21,11 +28,27 @@ export default function Header() {
             height={40}
           />
         </div>
-        <div className="links ">
+
+        <nav className="links desktop-toggle">
           <a href="">Home</a>
           <a href="">Search</a>
           <a href="">News</a>
-        </div>
+        </nav>
+
+        <button
+          className="menu-button mobile-toggle"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? <X size={24} /> : <Menu size={24} />}
+        </button>
+
+        {isOpen && (
+          <nav className="mobile-menu mobile-toggle links">
+            <a href="">Home</a>
+            <a href="">Search</a>
+            <a href="">News</a>
+          </nav>
+        )}
       </div>
     </header>
   );
